645de7fb6d0d07b4f8e7869eff84dc689c52606e
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-04-03 06:47:23 +0530

    ⚡ Refactor App component to use new layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { GlobalLayout } from "@/layouts/GlobalLayout";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./theme";
+import { CssBaseline } from "@mui/material";
+import { Header } from "./layouts/Header/Header";
+import { Outlet } from "react-router-dom";
+import { Footer } from "./layouts/Footer/Footer";
 
 function App() {
   return (
     <div className="app">
-      <GlobalLayout />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <Outlet />
+        <Footer />
+      </ThemeProvider>
     </div>
   );
 }
