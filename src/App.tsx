8a81c505cc0a4c00a216567a642dd8dbba6827cf
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-04-13 18:55:51 +0530

    ⚡ Refactor code structure and remove LoadingProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,15 @@ import { CssBaseline } from "@mui/material";
 import { Header } from "./layouts/Header/Header";
 import { Footer } from "./layouts/Footer/Footer";
 import { Content } from "./layouts/Content/Content";
-import { LoadingProvider } from "./context/LoadingProvider";
 
 function App() {
   return (
     <div className="app">
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <LoadingProvider>
-          <Header />
-          <Content />
-          <Footer />
-        </LoadingProvider>
+        <Header />
+        <Content />
+        <Footer />
       </ThemeProvider>
     </div>
   );
--- src/layouts/Content/Content.tsx
+++ src/layouts/Content/Content.tsx
@@ -1,20 +1,10 @@
-import { LoadingScreen } from "@/components/LoadingScreen";
-import { useLoading } from "@/hooks/useLoading";
 import { Grid } from "@mui/material";
-import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 export const Content = () => {
-  const { isLoading, setIsLoading } = useLoading();
-
-  useEffect(() => {
-    setIsLoading(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <Grid minHeight="100vh">
-      {isLoading ? <LoadingScreen isLoading /> : <Outlet />}
+      <Outlet />
     </Grid>
   );
 };
