1abef67f1a689c781b99fb7178d4a40639d69697
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-05-20 22:59:20 +0530

    ⚡Update function to handle edge cases

---
+++ merge.bat
@@ -0,0 +1,13 @@
+git fetch origin
+git checkout production
+git pull origin production
+git checkout development
+git pull origin development
+
+git checkout production
+git merge development
+
+
+git push origin production
+
+git checkout development
